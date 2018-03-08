b9689a0f36ffe13e6032170f505851878ccae5e4
ramapuramdilip <34623018+ramapuramdilip@users.noreply.github.com>
2018-03-08 19:25:58 +0530

    update percentage

--- src/vo/Student.java
+++ src/vo/Student.java
@@ -69,5 +69,7 @@ public class Student {
 public void setYearPassed(String yearPassed) {
 		this.yearPassed = yearPassed;
 	
-}
- 
+}public void setPercentage(String percentage) {
+		this.percentage = percentage;
+ public void setPercentage(String percentage) {
+		this.percentage = percentage;
